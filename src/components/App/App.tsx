fa172de8d33eaf946afa9b50a0c322cf1eeb1abe
VitaliiSakhno <vitalik.tarasovich@gmail.com>
2026-04-08 21:22:23 +0200

    implemented pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 import SearchBar from "../SearchBar/SearchBar";
@@ -7,6 +7,7 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
+import Pagination from "../Pagination/Pagination";
 
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
@@ -16,26 +17,30 @@ import toast, { Toaster } from "react-hot-toast";
 import "./App.module.css";
 
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const {data, isLoading, isError } = useQuery({
-    queryKey: ['movies', query],
-    queryFn: () => fetchMovies(query),
-    enabled: query !== ''
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["movies", query, currentPage],
+    queryFn: () => fetchMovies(query, currentPage),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
   });
 
-  const movies = data ?? [];
+  const movies = data?.results ?? [];
+  const totalPages = data?.total_pages ?? 0;
 
-  const handleSearch = async (query: string) => {
+  const handleSearch = (query: string) => {
     setQuery(query);
-
-    useEffect(() => {
-      if (data && movies.length === 0) {
-        toast('No movies found on your request');
-      }
-    }, [data, movies.length]);
+    setCurrentPage(1);
   };
+  
+  useEffect(() => {
+    if (data && movies.length === 0) {
+      toast('No movies found on your request');
+    }
+  }, [data]);
 
   const handleSelect = (movie: Movie) => {
     setSelectedMovie(movie);
@@ -56,6 +61,13 @@ function App() {
       {isError && <ErrorMessage />}
       {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={handleClose} />
+      )}
+      {data && totalPages > 1 && (
+        <Pagination
+          totalPages={totalPages}
+          page={currentPage}
+          setPage={setCurrentPage}
+        />
       )}
     </>
   );
